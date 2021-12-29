9626b9819a5a46d366e898216ad51d55b32ce8d2
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2021-12-29 04:10:33 -0500

    framework models

--- framework/models.py
+++ framework/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class user_changes(models.Models):
+
+class Superskill(models.Model):
+    meta_description = 
+    global_id = 
+    tags = #list #local_id
+    prerequisites = #list #local_id
+    subskills = #list #local_id
+    changes = #class
+
+class Subskill(models.Model):
+    meta_description =
+    global_id = 
+    tags = #list #local_id
+    prerequisites = #list #local_id
+    topics = #list #local_id
+    changes = #class
+    superskills_backlink = #list #global_id #local_id
