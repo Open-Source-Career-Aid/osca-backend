82adbf08b6fbc2cf9f73904160208cf4706b0405
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2021-12-29 04:52:04 -0500

    Framework update

--- framework/models.py
+++ framework/models.py
@@ -7,29 +7,26 @@ class user_changes(models.Models):
 class vote(models.Model):
 
 class Superskill(models.Model):
-    superskill_name = 
-    meta_description = 
-    global_id = 
+    superskill_name = models.CharField(max_length=100)
+    meta_description = models.CharField(max_length=200, blank=True)
     tags = #list #local_id
     prerequisites = #list #local_id
     subskills = #list #local_id
     timed_changes = #class
 
 class Subskill(models.Model):
-    subskill_name = 
-    meta_description =
-    global_id = 
-    tags = #list #local_id
-    prerequisites = #list #local_id
-    topics = #list #local_id
+    subskill_name = models.CharField(max_length=100)
+    meta_description = models.CharField(max_length=200, blank=True)
+    tags = models.ManyToManyField(Tag, related_name="subskill_tags", blank=True)
+    prerequisites = models.ManyToManyField(Prerequisite, related_name="subskill_prerequisites", blank=True)
+    topics = models.ManyToManyField(Topic, related_name="subskill_topics", blank=True)
     timed_changes = #class
     superskills_backlink = #list #global_id #local_id
     votes = #class
 
-class topic(models.Model):
-    tag_name = 
-    meta_description = 
-    global_id = 
+class Topic(models.Model):
+    tag_name = models.CharField(max_length=100)
+    meta_description = models.CharField(max_length=200, blank=True)
     tags =
     prerequisites = 
     topics = 
@@ -39,4 +36,8 @@ class topic(models.Model):
     topics_backlink =
     votes =
 
-class resource(models_model):
+class resource(models.Model):
+
+class Tag(models.Model):
+
+class Prerequisite(models.Model):
