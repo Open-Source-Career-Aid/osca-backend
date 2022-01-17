74ba6e5c8d2d1a4aeba51b1f2745d7225bb4da82
Mayank koshta <mayankkoshta@Mayanks-MacBook-Air.local>
2022-01-17 13:10:06 +0530

    new changes in model

--- framework/models.py
+++ framework/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-
+from .models import Vote
 # Create your models here.
 
 class user_changes(models.Models):
     pass
 
-class vote(models.Model):
-    pass
+# class vote(models.Model):
+#     pass
 
 
-class resource(models.Model):
+class Resource(models.Model):
     link = models.TextField(blank=True)
     # resource_vote = models.OneToOneField(Vote,default=0, related_name="vote_resource", parent_link=True, blank=True, on_delete=models.PROTECT)
     
@@ -30,30 +30,29 @@ class Prerequisite(models.Model):
         return self.prereqName
 
 
-class Topic(models.Model):
+class Topic(models.Model, Vote):
     tag_name = models.CharField(max_length=100)
     meta_description = models.CharField(max_length=200, blank=True)
     tags = models.ManyToManyField(Tag, related_name="topic_tags", blank=True)
     prerequisites = models.ManyToManyField(
         Prerequisite, related_name="topic_with_this_prerequisite", blank=True)
-    topics = models.CharField(max_length=100)
-    resources = #list #local_id
-    timed_changes = 
-    subskills_backlink = 
-    topics_backlink =
-    votes =
+    topic = models.CharField(max_length=100)
+    subtopics = models.ManyToManyField("self", on_delete=models.CASCADE, blank=True,  related_name="Subtopics", symmetrical=False)
+    resources = models.ManyToManyField(Resource, blank=True)
+    timed_changes = models.DateTimeField(auto_now_add=True)
+    # subskills_backlink = 
+    # topics_backlink =
 
-class Subskill(models.Model):
+class Subskill(models.Model, Vote):
     subskill_name = models.CharField(max_length=100)
     meta_description = models.CharField(max_length=200, blank=True)
     tags = models.ManyToManyField(Tag, related_name="subskill_tags", blank=True)
     prerequisites = models.ManyToManyField(Prerequisite, related_name="subskill_prerequisites", blank=True)
     topics = models.ManyToManyField(Topic, related_name="subskill_topics", blank=True)
-    timed_changes = #class
-    superskills_backlink = #list #global_id #local_id
-    votes = #class
+    timed_changes = models.DateTimeField(auto_now_add=True)
+    # superskills_backlink = models.ForeignKey(Superskill)
 
-class Superskill(models.Model):
+class Superskill(models.Model, Vote):
     superskill_name = models.CharField(max_length=100)
     meta_description = models.CharField(max_length=200, blank=True)
     tags = models.ManyToManyField(
@@ -62,7 +61,7 @@ class Superskill(models.Model):
         Prerequisite, related_name="all_skills_with_this_prerequisite", blank=True)
     subskills = models.ManyToManyField(
         Subskill, related_name="super_skill", blank=True)
-    timed_changes = #class
+    timed_changes = models.DateTimeField(auto_now_add=True)
 
 
 
